c4b2b6f4fc7b97fc5e7b1aa85c761f65504bc4ac
Pablo Guillermo <pgcb123@gmail.com>
2018-02-07 17:16:30 -0300

    Actividad nueva de Salas
    Agregué una actividad vacia que manejaria las salas de chat, por ahora son solo un par de botones sin funcionalidad ya que necesito seguir investigando un poco más como se labura en este ambiente de desarrollo

---
+++ app/src/main/java/ceballosvitalepabloguillermosacomanifrancogomeztoms/unichat/SalasChat.java
@@ -0,0 +1,13 @@
+package ceballosvitalepabloguillermosacomanifrancogomeztoms.unichat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SalasChat extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_salas_chat);
+    }
+}
